85577d1477ba39c1fea785ebb8a31d3083e65889
David Inoa <davidinoa@gmail.com>
2024-08-23 16:39:47 -0400

    feat: add marketing hero

--- app/(marketing)/header.tsx
+++ app/(marketing)/header.tsx
@@ -1,7 +1,44 @@
+import {
+  ClerkLoading,
+  ClerkLoaded,
+  SignedIn,
+  UserButton,
+  SignedOut,
+  SignInButton,
+} from '@clerk/nextjs'
+import Image from 'next/image'
+import { Loader } from 'lucide-react'
+import { Button } from '@/components/ui/button'
+
 export default function Header() {
   return (
     <header className="h-20 w-full border-b-2 border-slate-200 px-4">
-      Header
+      <div className="mx-auto flex h-full items-center justify-between lg:max-w-screen-lg">
+        <div className="flex items-center gap-x-3 pb-7 pl-4 pt-8">
+          <Image src="/mascot.svg" alt="logo" width={40} height={40} />
+          <h1 className="text-2xl font-extrabold tracking-wide text-green-600">
+            Lingo
+          </h1>
+        </div>
+        <ClerkLoading>
+          <Loader className="size-5 animate-spin text-muted-foreground" />
+        </ClerkLoading>
+        <ClerkLoaded>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+          <SignedOut>
+            <SignInButton
+              mode="modal"
+              forceRedirectUrl="/learn"
+              signUpForceRedirectUrl="/learn">
+              <Button size="lg" variant="ghost">
+                Sign In
+              </Button>
+            </SignInButton>
+          </SignedOut>
+        </ClerkLoaded>
+      </div>
     </header>
   )
 }
--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,3 +1,57 @@
+import { Button } from '@/components/ui/button'
+import {
+  ClerkLoaded,
+  ClerkLoading,
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+} from '@clerk/nextjs'
+import { Loader } from 'lucide-react'
+import Image from 'next/image'
+import Link from 'next/link'
+
 export default function MarketingPage() {
-  return <div>Hello Marketing!</div>
+  return (
+    <div className="mx-auto flex w-full max-w-[988px] flex-1 flex-col items-center justify-center gap-2 p-4 lg:flex-row">
+      <div className="relative mb-8 size-[240px] lg:mb-0 lg:size-[424px]">
+        <Image fill src="/hero.svg" alt="hero" />
+      </div>
+      <div className="flex flex-col items-center gap-y-8">
+        <h1 className="max-w-[480px] text-center text-xl font-bold text-neutral-600 lg:text-3xl">
+          Learn, practice, and master new languages with Lingo.
+        </h1>
+        <div>
+          <ClerkLoading>
+            <Loader className="size-5 animate-spin text-muted-foreground" />
+          </ClerkLoading>
+          <ClerkLoaded>
+            <SignedOut>
+              <SignUpButton
+                mode="modal"
+                forceRedirectUrl="/learn"
+                signInForceRedirectUrl="/learn">
+                <Button size="lg" variant="secondary" className="w-full">
+                  Get Started
+                </Button>
+              </SignUpButton>
+              <SignInButton
+                mode="modal"
+                forceRedirectUrl="/learn"
+                signUpForceRedirectUrl="/learn">
+                <Button size="lg" variant="primaryOutline" className="w-full">
+                  Get Started
+                </Button>
+              </SignInButton>
+            </SignedOut>
+            <SignedIn>
+              <Button asChild size="lg" variant="secondary" className="w-full">
+                <Link href="/learn">Continue Learning</Link>
+              </Button>
+            </SignedIn>
+          </ClerkLoaded>
+        </div>
+      </div>
+    </div>
+  )
 }
